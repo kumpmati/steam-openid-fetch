34736df5905a2aaf492478b10b76f3e0cd806c1d
Matias Kumpulainen <kumpulainen.matias@gmail.com>
2024-11-09 01:55:27 +0200

    fix: Remove all instances of Node.js API usage (#1)
    * formatting
    
    * replace node api usage with web equivalents

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,13 +1,16 @@
-declare module 'lightweight-openid' {
-    export class OpenIdClient {
-        authenticate(identifier: string, returnUrl: string): Promise<AuthUrl>;
-        validateResponse(responseUrl: string, returnUrl: string): Promise<ProviderResponse>;
-    }
+declare module "steam-openid-fetch" {
+  export class OpenIdClient {
+    authenticate(identifier: string, returnUrl: string): Promise<AuthUrl>;
+    validateResponse(
+      responseUrl: string,
+      returnUrl: string,
+    ): Promise<ProviderResponse>;
+  }
 
-    export type ProviderResponse = {
-        authenticated?: boolean;
-        claimedIdentifier?: string;
-    };
+  export type ProviderResponse = {
+    authenticated?: boolean;
+    claimedIdentifier?: string;
+  };
 
-    export type AuthUrl = string;
+  export type AuthUrl = string;
 }
